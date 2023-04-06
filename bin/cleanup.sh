e5d95d2a96f920988a051c5fa3e7ac8b898cd2e7
Tsbih Salman <2teasalman@gmail.com>
2023-04-06 13:02:01 -0400

    Created linux shell setup scripts.

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -4,7 +4,7 @@
 rm ~/.vimrc
 
 # to remove a line from my .bashrc file in my home directory
-sed -i 's/source ~\/.dotfiles\/bashrc custum//g' ~/.bashrc
+sed -i 's/source ~\/.dotfiles\/etc\/bashrc_custum//g' ~/.bashrc
 
 # to remove the .TRASH directory inside my home directory
 rm -r ~/.TRASH
